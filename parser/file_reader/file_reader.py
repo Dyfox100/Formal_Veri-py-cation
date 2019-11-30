953fc070e7efc9a508653ebf777fa4862377d6f5
Dylan Fox <dylan.fox@colorado.edu>
2019-11-30 14:46:10 -0700

    Completed File Reader, and added test file for reader

--- parser/file_reader/file_reader.py
+++ parser/file_reader/file_reader.py
@@ -8,9 +8,37 @@ class File_Reader(object):
 
     def __init__(self, path):
         self._path = path
-        if not os.path.isabs(self._path):
-            self._path = os.path.join(os.abspath(__main__.__file__), path)
+        #if not os.path.isabs(self._path):
+            #self._path = os.path.join(os.path.abspath(__main__.__file__), path)
 
     def read_file(self):
         file = open(self._path, "r")
-        
+
+        verification_blocks = []
+        current_verification_block = []
+        in_block = False
+
+        lines = file.readlines()
+
+
+        for line in lines:
+            line = line.strip()
+            #start verification comment
+            if "#FV" in line:
+                current_verification_block.append(line)
+                in_block = True
+            #end verification comment
+            elif "#END_FV" in line:
+                in_block = False
+                verification_blocks.append(current_verification_block)
+                current_verification_block = []
+            #in a verification command
+            elif in_block == True:
+                print("hit", current_verification_block)
+                current_verification_block.append(line)
+
+        return verification_blocks
+
+if __name__ == "__main__":
+    file_reader = File_Reader("test.py")
+    print(file_reader.read_file())
